6bab8fe864c60d4358bfe53d87be3f59fe8b79d6
kiumad <mirfatemi7@gmail.com>
2024-03-05 01:43:35 +0330

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     const expires = "expires=" + date.toUTCString();
     document.cookie =
-      name + "=" + value + "; " + expires + "; path=/third-party-cookie";
+      name + "=" + value + "; " + expires + "; path=/; domain=mamad.com";
   };
 
   thirdPartyCookie("MAMAD_AD_USER_ID", "FMNKJD4D48D8E52", 52);
